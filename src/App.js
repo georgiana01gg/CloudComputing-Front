dbc2b5e2e4590f876a561e85e6d159201e5d4012
georgianacrisan99@gmail.com <georgianacrisan99@gmail.com>
2022-05-05 18:35:59 +0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPAge from './JavaScript/components/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPAge />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/JavaScript/components/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainPage(props){
+    const {title}=props;
+    console.log(title);
+    return(
+        <div id="MainPage">
+
+        </div>
+    );
+};
+
+export default MainPAge;
